d5b8f034e1d3dddc6b933ed97358eb8b7d9c767b
Deano <8463497+newberdean@users.noreply.github.com>
2018-03-01 20:31:58 +1300

    My Test Project.
    MAN, getting this far was unnecessarily difficult.
    -_-
    
    Annnnnnnnnd now my email settings are foiling me...
    >_<

---
+++ Assets/Scripts/ScriptFile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptFile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("[INSERT FULL BEE MOVIE SCRIPT HERE.]");
+		Debug.Log ("... I honestly don't know why this is a meme.\nI mean, okay; I can " +
+			"see how having a wall of text scrolling at lightning speed might be funny. But " +
+			"*WHY* is it considered so funny?.\n\nI guess I don't understand the inner machinations " +
+			"of the Internet.\n\n\n\n...Actually, on balance I may be okay with this.");
+	}
+	
+	// Update is called once per frame - Which means we really shouldn't include it in a script if it's unused, right?
+	// I mean, it'll be a *fast* waste of time and processing power, but a waste is still a waste.
+	//void Update () {
+	//	
+	//}
+}
